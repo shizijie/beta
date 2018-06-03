d4f06b06d4cf273a1236d543c70ebd7fb56bbca6
xuliang <510013803@qq.com>
2018-06-03 18:39:44 +0800

    集成redis

--- service-beta/src/main/java/com/shizijie/beta/ServiceBetaApplication.java
+++ service-beta/src/main/java/com/shizijie/beta/ServiceBetaApplication.java
@@ -3,6 +3,7 @@ package com.shizijie.beta;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -10,7 +11,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
--- service-beta/src/main/java/com/shizijie/beta/config/TimerConfig.java
+++ service-beta/src/main/java/com/shizijie/beta/config/TimerConfig.java
@@ -2,6 +2,7 @@ package com.shizijie.beta.config;
 
 import com.shizijie.beta.auth.dao.UserDao;
 import com.shizijie.beta.auth.dto.UserDTO;
+import com.shizijie.beta.utils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,13 @@ import java.util.List;
 public class TimerConfig {
     @Autowired
     UserDao userDao;
+    @Autowired
+    RedisUtils redisUtils;
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    //@Scheduled(cron = "0/5 * * * * ?")
     void test(){
         System.out.println("1");
+        redisUtils.set("22","xl");
         List<UserDTO> list=userDao.getListUser();
         System.out.println("2");
     }
