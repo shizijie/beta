3f02dba89df8f3c28f743b24f759558c1bf7add2
xuliang <510013803@qq.com>
2018-06-24 18:24:22 +0800

    添加ci环境

--- service-beta/src/main/java/com/shizijie/beta/ServiceBetaApplication.java
+++ service-beta/src/main/java/com/shizijie/beta/ServiceBetaApplication.java
@@ -3,21 +3,19 @@ package com.shizijie.beta;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableHystrix
-@MapperScan("com.shizijie.beta.*")
+@MapperScan("com.shizijie.beta")
 @EnableScheduling
 @EnableTransactionManagement
 public class ServiceBetaApplication {
