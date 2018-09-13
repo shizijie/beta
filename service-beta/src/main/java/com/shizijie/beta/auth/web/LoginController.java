67d09f67867366edf49741b5ddbc8c9fd5538893
xuliang <510013803@qq.com>
2018-09-13 22:54:15 +0800

    add modify

--- service-beta/src/main/java/com/shizijie/beta/auth/web/LoginController.java
+++ service-beta/src/main/java/com/shizijie/beta/auth/web/LoginController.java
@@ -6,6 +6,8 @@ import com.shizijie.beta.auth.serivce.impl.UserServiceImpl;
 import com.shizijie.beta.auth.utils.MD5Util;
 import com.shizijie.beta.auth.vo.UserVO;
 import com.shizijie.beta.common.ResultBean;
+import com.shizijie.beta.thread.BetaThreadPoolServices;
+import com.shizijie.beta.thread.KpiCalcThread;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.*;
 
 @Api(value="user", tags="用户模块")
 @RestController
@@ -44,8 +47,53 @@ public class LoginController {
     public ResultBean test(UserVO vo){
         //EnumServiceImpl.valueOf("LOGIN").test("xl",userDao);
         System.out.println("start");
-        userServiceImpl.test();
+        ThreadPoolExecutor pool=BetaThreadPoolServices.getThreadPool("TEST",4);
+        for(int i=0;i<10;i++){
+            Future future=pool.submit(new KpiCalcThread(i));
+        }
+        pool.shutdown();
+        try {
+            System.out.println("=============="+pool.awaitTermination(1, TimeUnit.DAYS));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(pool.isShutdown());
+        System.out.println(pool.isTerminated());
         System.out.println("end");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(pool);
+
+        CompletableFuture<String> completableFuture=new CompletableFuture();
+        new Thread(new Runnable() {
+            @Override public void run() {
+                //模拟执行耗时任务
+                System.out.println("task doing...");
+                try {
+                    Thread.sleep(3000);
+                }
+                catch (InterruptedException e) { e.printStackTrace(); }
+                //告诉completableFuture任务已经完成
+                completableFuture.complete("result");
+            }
+        }).start();
+        //获取任务结果，如果没有完成会一直阻塞等待
+        String result= null;
+        try {
+            result = completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("计算结果:"+result);
+
+
+
+
         return ResultBean.success("ok");
     }
 
